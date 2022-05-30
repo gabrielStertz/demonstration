e3dc8f7bc61b5ddc5d31587f66b880f387e8034e
Gabriel Stertz <gabrielstertz2012@gmail.com>
2022-05-30 08:51:33 -0300

    adding Array.js

---
+++ array.js
@@ -0,0 +1,11 @@
+/*
+   Array (Vetores)
+   - Uma lista
+   - Agrupamento de dado
+
+
+   ['Gabriel', 39]
+*/
+console.log(['Gabriel', 39])
+console.log(["ovos", "leite", "Pão", "açucar",
+            2, 3])
