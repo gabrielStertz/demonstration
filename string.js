c35f05b134c480dbd433ed938870e4884ee61f16
Gabriel Stertz <gabrielstertz2012@gmail.com>
2022-05-30 08:55:57 -0300

    adding string.js

---
+++ string.js
@@ -0,0 +1,14 @@
+/*String
+    cadeia de caracteres
+
+    "" - aspas duplas
+    '' - aspas simples
+    `` - template literals ou template strings
+*/
+console.log('Posso colocar "aspas duplas"')
+console.log("Posso colocar 'aspas simples'")
+console.log(`posso colocar 'aspas simples'
+posso colocar "aspas duplas" e fazer 
+multilinhas`)
+console.log(`posso colocar expressões ${1 + 1}`)
+console.log(1 + 1)
